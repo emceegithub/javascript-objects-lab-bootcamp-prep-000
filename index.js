3d24613f4c6bc8a1c1564558286491ff5ff47372
Mark Cruz <markchriscruz@yahoo.com>
2018-02-13 05:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObject
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
