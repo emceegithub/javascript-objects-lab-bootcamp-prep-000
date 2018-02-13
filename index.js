b42b7ffe73616273ff52069544659f6add8d0859
Mark Cruz <markchriscruz@yahoo.com>
2018-02-13 05:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, object)
+  newObject[key] = value
+  return newObject
+}
+
